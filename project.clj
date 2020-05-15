bcff4136d92d027699be38a2abde843281e610d6
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2020-05-15 20:18:08 +0400

    0.0.23

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject darkleaf/effect "0.0.22"
+(defproject darkleaf/effect "0.0.23"
   :description "Effect system"
   :url "https://github.com/darkleaf/effect/"
   :license {:name "Eclipse Public License"
